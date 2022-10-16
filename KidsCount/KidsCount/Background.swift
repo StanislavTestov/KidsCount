7323d4204980713e2c14e50e34c8854b62e5c4e7
Stanislav <te100ff@inbox.ru>
2022-10-16 21:24:33 +0400

    gradient complete

--- KidsCount/KidsCount/Background.swift
+++ KidsCount/KidsCount/Background.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct Background: View {
     var body: some View {
-        LinearGradient(colors: [.yellow, .green], startPoint: .top, endPoint: .bottom)
+        LinearGradient(
+            stops: [
+                .init(color: .yellow, location: 0.65),
+                .init(color: .green, location: 0.95)
+            ],
+            startPoint: .top,
+            endPoint: .bottom
+        )
+            .ignoresSafeArea()
+            .backgroundStyle(.ultraThickMaterial)
+       
     }
 }
 
