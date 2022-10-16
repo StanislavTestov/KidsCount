d7e7042aa509bb12fe8c1fa1a83dc5f945e413e1
Stanislav <te100ff@inbox.ru>
2022-10-16 22:21:18 +0400

    Start creating UI of counting numbers

---
+++ KidsCount/KidsCount/NumbersForCount.swift
@@ -0,0 +1,27 @@
+//
+//  NumbersForCount.swift
+//  KidsCount
+//
+//  Created by Stanislav Testov on 16.10.2022.
+//
+
+import SwiftUI
+
+struct NumbersForCount: View {
+    let firstNumber = 3
+    let secondNumber = 7
+    let operation = Operations.plus("+")
+    
+    var body: some View {
+        HStack {
+            Text(String(firstNumber))
+        }
+        
+    }
+}
+
+struct NumbersForCount_Previews: PreviewProvider {
+    static var previews: some View {
+        NumbersForCount()
+    }
+}
---
+++ KidsCount/KidsCount/Operations.swift
@@ -0,0 +1,11 @@
+//
+//  Operations.swift
+//  KidsCount
+//
+//  Created by Stanislav Testov on 16.10.2022.
+//
+
+enum Operations {
+    case plus(String)
+    case minus(String)
+}
